a5fe201f3042e0c50e84606ade05e2ed538e6c01
Animesh170199 <animesh@strivemindz.com>
2022-06-24 14:19:11 +0530

    first commit

---
+++ Hulu App/hulu-animesh/utils/requests.js
@@ -0,0 +1,31 @@
+const API_KEY = process.env.API_KEY;
+export default {
+  fetchTrending: {
+    title: "Trending",
+    url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchTopRated: {
+    title: "Top Rated",
+    url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  },
+  fetchActionMovies: {
+    title: "Action",
+    url: `/discover/movie/?api_key=${API_KEY}&with_geners=28`,
+  },
+  fetchComedyMovies: {
+    title: "Comedy",
+    url: `/discover/movie/?api_key=${API_KEY}&with_geners=35`,
+  },
+  fetchHorrorMovies: {
+    title: "Horror",
+    url: `/discover/movie/?api_key=${API_KEY}&with_geners=27`,
+  },
+  fetchRomanceMovies: {
+    title: "Romance",
+    url: `/discover/movie/?api_key=${API_KEY}&with_geners=10749`,
+  },
+  fetchMystery: {
+    title: "Mystery",
+    url: `/discover/movie/?api_key=${API_KEY}&with_geners=9648`,
+  },
+};
